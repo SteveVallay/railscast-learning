9f07592578629474fb146e28d1d9929c610d645e
zhibinw <zhibinwang.q@gmail.com>
2013-10-21 10:47:11 +0800

    add new app

---
+++ 20_restricting_access/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c5c07e1c26b3da6527c28cf5ed5b295061aff75e8360cff024347870fb2f78d0e7a609a9c4cfec33ee88cae5a8af0023ab5cf1f48e1df0f9bbfc257aa57da34f'
